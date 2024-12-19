6b8aa267f68ef2cb75565d9deac3159ffb53402f
QuangMinhPhan23 <147715883+QuangMinhPhan23@users.noreply.github.com>
2024-12-19 19:37:13 +0700

    Update login.py

--- login.py
+++ login.py
@@ -7,7 +7,7 @@ import mysql.connector
 server = '127.0.0.1'
 db = 'minimart'
 username = 'root'
-pwd = '27052306aA$'
+pwd = ''
 conn = mysql.connector.connect(host=server, database=db, user=username, password=pwd, use_unicode=True, charset="utf8")
 cur = conn.cursor()
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QPushButton, QHBoxLayout, QWidget, QMessageBox
